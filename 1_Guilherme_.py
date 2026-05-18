50c6c1d614dfb11740672845c9f0aada95e581ed
Paulo Cézar <maiapaulo783@gmail.com>
2026-05-18 14:18:15 -0300

    Atualização do projeto

--- 1_Guilherme_.py
+++ 1_Guilherme_.py
@@ -1,2 +1,16 @@
-print('--- SEJA BEM VINDO AO SIMULADOR DE DECIOSÕES ')
+print('--- SEJA BEM VINDO AO SIMULADOR DE DECISÕES ---')
 
+input("Aperte ENTER para continuar...")
+
+def Iniciar():
+    nome = input('Defina o nome do seu jogador: ')
+    
+    print(f'Seja muito bem-vindo {nome}. Você está começando sua carreira agora e vai precisar tomar decisões para ter sucesso.')
+    return nome
+
+def Comeco(nome):
+    print(f'Olá {nome}. Você é um jogador da base do flamengo e esta prestes a começar a sua carreira\n mas antes voce precisa saber dos seus atributos como jopgador.\n 1- Habilidade =50\n 2- Dinheiro = 1000\n 3- Energia = 100\n 4- Fama = 0\n 5- idade =  17 ')
+
+
+jogador = Iniciar()
+Comeco(jogador)
