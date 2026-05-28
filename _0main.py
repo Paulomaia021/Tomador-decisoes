edc3ecc8378bdda0ad9072e28c794be5c98e3089
Paulo Cézar <maiapaulo783@gmail.com>
2026-05-28 00:57:12 -0300

    Novo arquivo main

--- _0main.py
+++ _0main.py
@@ -1,3 +1,49 @@
-from _1Guilherme import  Iniciar 
-from _1Guilherme import  decisao1
-from _1Guilherme import Comeco
+# Simulador: [Simulador de carreira]
+# Grupo: Paulo Cézar, Kaue Souza, Guilherme Fonseca
+from _1Guilherme import Iniciar, decisao1,chegada
+
+print('--- SEJA BEM VINDO AO SIMULADOR DE CARREIRA  ---')
+print('Você tem exatamete 10 dias pra construir o seu jogador com objetivo de ir pra copa do mundo')
+input("\nAperte ENTER para continuar...")
+
+atributos = {
+    'habilidade': 50,
+    'energia': 100,
+    'fama': 0,
+    'dia': 1,
+    'reputacao': 50
+}
+
+
+
+def ciclo():
+    while True:
+        print('\nO que você quer fazer?:\n'
+              '1 - Jogar\n'
+              '2 - Não jogar, para ir treinar')
+        
+        jogou = input('Escolha um número: ')
+        
+        if jogou == '1':
+            print('\nMeus parabéns, o seu time ganhou o jogo e a equipe técnica disse que um olheiro de time grande gostou de você!')
+            print('Você ganhou +50 fama e perdeu -25 de energia.')
+            atributos['energia'] -= 25
+            atributos['fama'] += 50
+            break  
+            
+        elif jogou == '2':
+            print('\nVocê perdeu uma grande oportunidade...') 
+            input('Aperte ENTER para continuar...') 
+            print('Você perdeu a chance de ir para a copa. O jogo acabou.')
+            break
+            
+        else:
+           
+            print('\n[ERRO] Opção inválida! Digite apenas 1 ou 2.')
+            input('Aperte ENTER para tentar novamente...') 
+
+
+    Iniciar()
+    decisao1()
+    chegada(atributos)
+    print(f'\nAtributos atuais: {atributos}')
