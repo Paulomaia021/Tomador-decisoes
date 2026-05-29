009e2b51d9bcaddf30c1f569195f8c3349669085
Paulo Cézar <maiapaulo783@gmail.com>
2026-05-28 23:27:12 -0300

    Codigo atualizado e funcionando

--- _0main.py
+++ _0main.py
@@ -1,11 +1,13 @@
 # Simulador: [Simulador de carreira]
 # Grupo: Paulo Cézar, Kaue Souza, Guilherme Fonseca
-from _1Guilherme import Iniciar, decisao1,chegada
+from _1Guilherme import Iniciar, decisao1, chegada
 
+# 1. Mensagem de Boas-Vindas inicial do jogo
 print('--- SEJA BEM VINDO AO SIMULADOR DE CARREIRA  ---')
-print('Você tem exatamete 10 dias pra construir o seu jogador com objetivo de ir pra copa do mundo')
-input("\nAperte ENTER para continuar...")
+print('Você tem exatamente 10 dias pra construir o seu jogador com objetivo de ir pra copa do mundo')
+input("\nAperte ENTER para continuar...\n")
 
+# 2. Dicionário de atributos inicializado
 atributos = {
     'habilidade': 50,
     'energia': 100,
@@ -14,36 +16,15 @@ atributos = {
     'reputacao': 50
 }
 
+# 3. Execução da história do jogo na ordem correta
+# Pede o nome do jogador
+Iniciar() 
 
+# Realiza a primeira grande decisão (Jogo contra Arthur Nogueira)
+decisao1() 
 
-def ciclo():
-    while True:
-        print('\nO que você quer fazer?:\n'
-              '1 - Jogar\n'
-              '2 - Não jogar, para ir treinar')
-        
-        jogou = input('Escolha um número: ')
-        
-        if jogou == '1':
-            print('\nMeus parabéns, o seu time ganhou o jogo e a equipe técnica disse que um olheiro de time grande gostou de você!')
-            print('Você ganhou +50 fama e perdeu -25 de energia.')
-            atributos['energia'] -= 25
-            atributos['fama'] += 50
-            break  
-            
-        elif jogou == '2':
-            print('\nVocê perdeu uma grande oportunidade...') 
-            input('Aperte ENTER para continuar...') 
-            print('Você perdeu a chance de ir para a copa. O jogo acabou.')
-            break
-            
-        else:
-           
-            print('\n[ERRO] Opção inválida! Digite apenas 1 ou 2.')
-            input('Aperte ENTER para tentar novamente...') 
+# Mostra a transferência para o Palmeiras e atualiza os atributos passados por parâmetro
+chegada(atributos) 
 
-
-    Iniciar()
-    decisao1()
-    chegada(atributos)
-    print(f'\nAtributos atuais: {atributos}')
+# 4. Mostra o placar de atributos finalizado do dia
+print(f'\nAtributos atuais do jogador: {atributos}')
--- _1Guilherme.py
+++ _1Guilherme.py
@@ -9,16 +9,15 @@ def Iniciar():
             print('[ERRO] O nome deve conter apenas letras! Tente novamente.')
         else:
             break
-    print('oi maçgggggggggabuceta')
+
     print(f'Seja muito bem-vindo {nome}. Você está começando sua carreira agora e vai precisar tomar decisões para ter sucesso.')
 
-#Aqui aonde começa a primeira decisão feita pelo usuário 
 def decisao1():
-  input("Aperte ENTER para continuar...\n")
-    
-print('Hoje é mais um dia normal de jogo contra Arthur nogueira, porém o tecnico te deu a opção de não jogar pra ficar treinando hoje se quiser')
+
+    input("Aperte ENTER para continuar...\n")
+    print('Hoje é mais um dia normal de jogo contra Arthur nogueira, porém o tecnico te deu a opção de não jogar pra ficar treinando hoje se quiser')
      
-while True:
+    while True:
         print('\nO que você quer fazer?:\n'
               '1 - Jogar\n'
               '2 - Não jogar, para ir treinar')
@@ -35,22 +34,16 @@ while True:
             input('Aperte ENTER para continuar...') 
             print('Você perdeu a chance de ir para a copa. O jogo acabou.')
             break
-            
         else:
-           
             print('\n[ERRO] Opção inválida! Digite apenas 1 ou 2.')
             input('Aperte ENTER para tentar novamente...') 
 
-        print('Hoje é mais um dia normal de jogo contra Arthur nogueira, porém o tecnico te deu a opção de não jogar pra ficar treinando hoje se quiser')
-
 def chegada(atributos):
+    input('Aperte ENTER para tentar novamente...') 
+    
     print('Você chegou no palmeiras e ja foi convocado para jogar um jogo')
     print('Você ganhou mais 15+ habilidade, 30+ fama, 20- energia')
     atributos["habilidade"] += 15
     atributos["fama"] += 30
     atributos["energia"] -= 20
-input("\nAperte ENTER para continuar...")
-
-Iniciar()
-decisao1()
-chegada()
+    input("\nAperte ENTER para continuar...") 
